7894979a2e062d3bcb68be507045416f4be23268
Gustavo Franco <gustavofrancojp@gmail.com>
2017-11-12 20:05:45 -0400

    Att bug

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,9 @@
             <td>{{$registro->descricao}}</td>
             <td>{{$registro->capacidade}}</td>
             <td>
-              <a style="color: #ff9800;" href="{{route('admin.auditorio.editar', $registro->id)}}"><i class="small material-icons">create</i></a>
+              <a style="color: #ff9800;" href="#modal-editar">
+                <i class="small material-icons">create</i>
+              </a>
               <a style="color: #F44336;" href="{{route('admin.auditorio.deletar', $registro->id)}}"><i class="small material-icons">delete</i></a>
               <a style="color: #43A047;" href="{{route('agendamento.agendar', $registro->id)}}"><i class="small material-icons">date_range</i></a>
             </td>
@@ -38,17 +40,34 @@
     </table>
   </div>
   <div class="row">
-    <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Adicionar</a>
+    <a class="waves-effect waves-light btn modal-trigger" href="#modal-adicionar">Adicionar</a>
 
-    <!-- Modal Structure -->
-    <div id="modal1" class="modal">
+    <!-- Modal Adicionar -->
+    <div id="modal-adicionar" class="modal">
       <div class="modal-content">
         <h4>Modal Header</h4>
         <form class="" action="{{route('admin.auditorio.salvar')}}" method="post">
           {{ csrf_field() }}
-          @include('admin._form')
+          @include('admin._form-adicionar')
 
           <button class="btn deep-orange">Salvar</button>
+
+        </form>
+      </div>
+      <div class="modal-footer">
+        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+      </div>
+    </div>
+    <!-- Modal Editar -->
+    <div id="modal-editar" class="modal">
+      <div class="modal-content">
+        <h4>Modal Header</h4>
+        <form class="" action="{{route('admin.auditorio.atualizar', $registro->id)}}" method="post" enctype="multipart/form-data">
+          {{ csrf_field() }}
+          <input type="hidden" name="_method" value="put">
+          @include('admin._form')
+
+          <button class="btn deep-orange">Atualizar</button>
 
         </form>
       </div>
