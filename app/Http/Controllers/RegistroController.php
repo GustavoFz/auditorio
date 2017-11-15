c39d776f4b3cd30bd370e9ef9f9a5bf81bbd3fb7
Andrhe Victor <andrhevictor@gmail.com>
2017-11-15 15:52:56 -0400

    criado controller de registro
    criado controller de registro

---
+++ app/Http/Controllers/RegistroController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegistroController extends Controller
+{
+    public function index(){
+    	
+    	return view('registro');
+    }
+
+    public function registrar(){
+    	
+    	return view('welcome');
+    }
+}
---
+++ resources/views/registro.blade.php
@@ -0,0 +1,44 @@
+   @extends('layout.site')
+
+@section('titulo', 'Registro')
+
+@section('conteudo')
+
+<div class="container">
+    <form class="form-horizontal" method="POST" action="#">
+        {{ csrf_field() }}
+        <div class="row">
+        <div class="input-field col s12">
+            <i class="material-icons prefix">person</i>
+            <input id="email" name="email" type="email" class="validate">
+            <label for="email">Email</label>
+        </div>
+    </div>
+    <div class="row">
+        <div class="input-field col s12">
+            <i class="material-icons prefix">lock</i>
+            <input id="password" name="password" type="password" class="validate">
+            <label for="password">Password</label>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s6">
+            <div class="col s6"
+            <p class="left-align medium-small">
+                <a href="#">Esqueceu a senha?</a>
+            </p>
+        </div>
+
+        <input type="checkbox" class="filled-in checkbox-custom" id="filled-in-box" checked="checked"/>
+        <label for="filled-in-box">Lembrar senha</label>
+
+    </div>
+    <div class="col s6">
+        <button class="btn waves-effect waves-light right" type="submit" name="action">Entrar
+            <i class="material-icons right">send</i>
+        </button>
+    </div>
+
+    </form>
+</div>
+@endsection
