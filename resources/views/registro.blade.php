8c54059950fc90599d2c48472e660462be8061f2
Andrhe Victor <andrhevictor@gmail.com>
2017-11-18 19:03:02 -0400

    formulario de registro

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -5,40 +5,68 @@
 @section('conteudo')
 
 <div class="container">
-    <form class="form-horizontal" method="POST" action="#">
-        {{ csrf_field() }}
-        <div class="row">
-        <div class="input-field col s12">
-            <i class="material-icons prefix">person</i>
-            <input id="email" name="email" type="email" class="validate">
-            <label for="email">Email</label>
-        </div>
-    </div>
-    <div class="row">
-        <div class="input-field col s12">
-            <i class="material-icons prefix">lock</i>
-            <input id="password" name="password" type="password" class="validate">
-            <label for="password">Password</label>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col s6">
-            <div class="col s6"
-            <p class="left-align medium-small">
-                <a href="#">Esqueceu a senha?</a>
-            </p>
-        </div>
+     <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{route('site.registrar')}}">
+                        {{ csrf_field() }}
 
-        <input type="checkbox" class="filled-in checkbox-custom" id="filled-in-box" checked="checked"/>
-        <label for="filled-in-box">Lembrar senha</label>
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">Name</label>
 
-    </div>
-    <div class="col s6">
-        <button class="btn waves-effect waves-light right" type="submit" name="action">Entrar
-            <i class="material-icons right">send</i>
-        </button>
-    </div>
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
-    </form>
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
 </div>
 @endsection
