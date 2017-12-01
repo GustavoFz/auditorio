9fedd6e7fb47239f81d9c08cd1f9cf83614270af
Andrhé Victor <andrhevictor@gmail.com>
2017-12-01 11:11:21 -0400

    logout e register routes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,23 @@
 @section('conteudo')
 <div class="container">
 
-	 @if(Auth::guest())
-	            <h3 class="center-align">Bem-vindo!</h3>
-	            <p class="center-align">Para efetuar um agendamento, faça login</p> 
-	          @else
-				<h3 class="center-align">Bem-vindo, {{ Auth::user()->name }}!</h3>
-	          @endif
+	@if(Auth::guest())
+	<h3 class="center-align">Bem-vindo!</h3>
+	<p class="center-align">Para efetuar um agendamento, faça login</p> 
+	@else
+	<h3 class="center-align">Bem-vindo, {{ Auth::user()->name }}!</h3>
+	<a href="{{ route('logout') }}"
+	onclick="event.preventDefault();
+	document.getElementById('logout-form').submit();">
+	Logout
+	</a>
+
+	<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+		{{ csrf_field() }}
+	</form>
+@endif
+
+
+
 </div>
 @endsection
