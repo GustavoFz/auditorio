50014d17c027dca7fd527a44be9d42fb6ca0e226
Andrhe Victor <andrhevictor@gmail.com>
2017-12-02 15:07:02 -0400

    tirar logout do welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,16 +14,6 @@
 	@foreach(Auth::user()->habilidades() as $habilidade)
     	{{$habilidade->name}}<br>
     @endforeach
-	
-    <a href="{{ route('logout') }}"
-    onclick="event.preventDefault();
-    document.getElementById('logout-form').submit();">
-    Logout
-	</a>
-
-	<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-		{{ csrf_field() }}
-	</form>
 @endif
 
 
