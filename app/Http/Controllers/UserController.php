3041d3f27acf8bbab5b9879bc389490b44df203f
Andrhe Victor <andrhevictor@gmail.com>
2017-12-02 20:28:36 -0400

    authorize

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,13 +11,9 @@ use Illuminate\Support\Facades\DB;
 class UserController extends Controller
 {
     public function index(){
-    	if (Bouncer::is(Auth::user())->an('admin')){
+    	 $this->authorize('edit', User::class);
     		$usuarios = User::all();
     		$roles = DB::table('roles')->select('name')->get();
     		return view ('usuarios', compact('usuarios', 'roles'));
-    	} else {
-    		return view ('errors.403');
-    	}
-
     }
 }
