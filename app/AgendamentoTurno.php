35cd2b7f53df83d66e18e5af2018656e0dbc2364
Andrhé Victor <andrhevictor@gmail.com>
2017-12-04 15:59:39 -0400

    turno

--- app/AgendamentoTurno.php
+++ app/AgendamentoTurno.php
@@ -3,10 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Agendamento;
 
 class AgendamentoTurno extends Model
 {
     protected $fillable = [
     	'agendamento_id','turno', 'status'
     ];
+
+    public function agendamento(){
+      return $this->belongsTo(Agendamento::class, 'agendamento_id', 'id');
+      //Caso o nome da coluna seja diferente, passa ela por parametro
+    // return $this->belongsTo(Country::class, 'country_id', 'id');
+    }
 }
